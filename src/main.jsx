212bc2d4698a15f3ed634a811da042d3839a4f6d
sejinbaek <sejin9767@gmail.com>
2025-04-29 09:46:24 +0900

    📦chore: tanstack query 기본 설정 추가

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,16 @@ import '@/assets/index.css'
 import MainLayout from './layout/mainLayout'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router/index.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
+const queryClient = new QueryClient()
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      {/* 리액트 쿼리 DevTools이 개발환경에서만 표시되게 해달라는 것 */}
+      {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+    </QueryClientProvider>
   </StrictMode>
 )
