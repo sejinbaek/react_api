32bceb74bb3d7d1f1b915855c7b2a489afc8c1f1
sejinbaek <sejin9767@gmail.com>
2025-04-29 10:49:35 +0900

    ✨ feat: 야영장 페이지 초기 구조 생성

---
+++ src/camping/CampingPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const CampingPage = () => {
+  return (
+    <main>
+      <h2>CampingPage</h2>
+      <div>
+        <p>총 4137개 중 10개 표시 / 현재 1페이지에 있음</p>
+        <ul>
+          <li>
+            <p>야영장명</p>
+            <p>주소</p>
+            <p>전화번호</p>
+          </li>
+        </ul>
+      </div>
+    </main>
+  )
+}
+
+export default CampingPage
--- src/layout/MenuList.jsx
+++ src/layout/MenuList.jsx
@@ -5,13 +5,26 @@ import css from './MenuList.module.css'
 const MenuList = () => {
   return (
     <ul>
-      <li className={css.list}>
-        <NavLink to={'/'} className={isActive => (isActive ? `${css.active}` : '')}>
-          날씨 API 활용
-        </NavLink>
-      </li>
+      <CustomNavLink
+        to={'/'}
+        label={'날씨 API 활용'}
+        className={isActive => (isActive ? `${css.active}` : '')}
+      />
+      <CustomNavLink
+        to={'/camping'}
+        label={'야영장 API 활용'}
+        className={isActive => (isActive ? `${css.active}` : '')}
+      />
     </ul>
   )
 }
+
+const CustomNavLink = ({ to, label }) => (
+  <li className={css.list}>
+    <NavLink to={to} className={isActive => (isActive ? `${css.active}` : '')}>
+      {label}
+    </NavLink>
+  </li>
+)
 
 export default MenuList
